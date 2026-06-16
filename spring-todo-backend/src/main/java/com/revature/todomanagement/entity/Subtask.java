a834f6ccdf4461e066f908964fc0ba1316133b20
jelainhart <jelainhart@gmail.com>
2026-06-16 15:29:00 -0400

    added task and subtask entities

--- spring-todo-backend/src/main/java/com/revature/todomanagement/entity/Subtask.java
+++ spring-todo-backend/src/main/java/com/revature/todomanagement/entity/Subtask.java
@@ -1,5 +1,30 @@
 package com.revature.todomanagement.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "subtasks")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Subtask {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column
+    private UUID id;
+
+    @Column(nullable = false)
+    private UUID taskId;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private boolean completed = false;
 }
