eabfb11afc80f4072858c6c2826e27835ec6e3ce
jelainhart <jelainhart@gmail.com>
2026-06-16 15:13:55 -0400

    added the User entity

--- spring-todo-backend/src/main/java/com/revature/todomanagement/entity/User.java
+++ spring-todo-backend/src/main/java/com/revature/todomanagement/entity/User.java
@@ -1,5 +1,27 @@
 package com.revature.todomanagement.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
-    
+
+    @Column
+    @Id
+    @GeneratedValue (strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
 }
